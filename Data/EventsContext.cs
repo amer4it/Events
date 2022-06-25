a4c3a55c79d241108f7828caaf71406c6aafead2
amer <adiga_man@hotmail.co.uk>
2022-06-25 20:39:21 +0300

    Add project files.

---
+++ Data/EventsContext.cs
@@ -0,0 +1,25 @@
+﻿using Events.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Events.Data
+{
+    public class EventsContext:DbContext
+    {
+        public EventsContext(DbContextOptions<EventsContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Event> Courses { get; set; }
+        public DbSet<RelatedEvent> Enrollments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Event>().ToTable("Event");
+            modelBuilder.Entity<RelatedEvent>().ToTable("RelatedEvent");
+        }
+    }
+}
---
+++ Models/Event.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Events.Models
+{
+    public class Event
+    {
+        public int Id { set; get; }
+        public string Title { set; get; }
+        public string Description { set; get; }
+        public string Location { set; get; }
+        public string ArabicTitle { set; get; }
+        public string ArabicDescription { set; get; }
+        public string ArabicLocation { set; get; }
+        public int CoordLongitude { set; get; }
+        public int CoordLatitude { set; get; }
+        public string ImagePath { set; get; }
+        public DateTime StartDate { set; get; }
+        public ICollection<RelatedEvent> RelatedEvents { set; get; }
+    }
+}
